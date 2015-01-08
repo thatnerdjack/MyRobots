4250f12c320f2aa17f166e179f06e0dcf20a4464
thatnerdjack <jackjyro@gmail.com>
2015-01-08 14:15:12 -0500

    WIP - working on robot targeting

--- src/jackdoherty/robo/MyRobotIsBetterThanYours.java
+++ src/jackdoherty/robo/MyRobotIsBetterThanYours.java
@@ -2,6 +2,7 @@ package jackdoherty.robo;
 import robocode.*;
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Random;
 
 // API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
 
@@ -10,35 +11,58 @@ import java.util.ArrayList;
  */
 public class MyRobotIsBetterThanYours extends Robot {
 
-	private boolean isLooking = false;
-	private double bearing = -1;
+	private double radarHit = -1;
 	private boolean isMovingRight = false;
-	private ScannedRobotEvent enemyRobot;
 	private final Color[] defaultColors = {
-		Color.red, Color.blue, Color.green, Color.magenta, Color.green
-	};
+		Color.red, Color.blue, Color.green, Color.magenta, Color.green, Color.blue
+	}; //body, gun, radar, bullet, radar arc, scan color
 
 	public void run() {
+		setAdjustGunForRobotTurn(true);
+		setAdjustRadarForGunTurn(true);
+		setAdjustRadarForRobotTurn(true);
+
 		while(true) {
-			arraySetColors(defaultColors);
-			if(!isLooking) {
-				turnRadarRight(360);
-			} else {
-				back(100);
-			}
+			setDefaultColors(defaultColors);
+			ahead(100);
+			turnRight(90);
+			turnRadarRight(360);
 		}
 	}
 
-	public void arraySetColors(Color[] colors) {
+	private void setGunHeading(double heading){
+		double dif = heading - getGunHeading();
+
+		if(dif < 180){
+			turnGunRight(dif);
+		} else{
+			turnGunLeft(dif);
+		}
+	}
+
+	private void setDefaultColors(Color[] colors) {
 		setColors(defaultColors[0], defaultColors[1], defaultColors[2], defaultColors[3], defaultColors[4]); // body,gun,radar, bullet, radar arc
+		setScanColor(defaultColors[5]);
+	}
+
+	private double getRandomDouble(double min, double max) {
+		Random r = new Random();
+		double randomValue = min + (max - min) * r.nextDouble();
+		return randomValue;
+	}
+
+	private void roboFire(double min, double max) {
+		setBulletColor(defaultColors[(int)getRandomDouble(0, defaultColors.length)]);
+		fire(getRandomDouble(min, max));
 	}
 
 	/**
 	 * onScannedRobot: What to do when you see another robot
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
-		enemyRobot = e;
-		fire(1);
+		radarHit = e.getBearing();
+		setGunHeading(radarHit);
+		roboFire(0, 10);
 	}
 
 	/**
